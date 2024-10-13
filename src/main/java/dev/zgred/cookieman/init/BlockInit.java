7fb50875b68aa6732b02e96eadcda01ea21678a2
kemfo4 <kemfomate@gmail.com>
2024-10-13 11:20:02 +0000

    Nowe Bloki

--- src/main/java/dev/zgred/cookieman/init/BlockInit.java
+++ src/main/java/dev/zgred/cookieman/init/BlockInit.java
@@ -24,4 +24,53 @@ public class BlockInit {
                     .strength(0.9f)
                     .dynamicShape()
                     .forceSolidOn()));
+
+    public static final RegistryObject<Block> COOKIE_STONE = BLOCKS.register( "cookie_stone",
+            () -> new Block(BlockBehaviour.Properties.of()
+                    .mapColor(MapColor.STONE)
+                    .strength(10.0f)
+                    .dynamicShape()
+                    .forceSolidOn()));
+
+    public static final RegistryObject<Block> COOKIE_LOG = BLOCKS.register( "cookie_log",
+            () -> new Block(BlockBehaviour.Properties.of()
+                    .mapColor(MapColor.STONE)
+                    .strength(2.0f)
+                    .dynamicShape()
+                    .forceSolidOn()));
+
+    public static final RegistryObject<Block> COOKIE_WOOD = BLOCKS.register( "cookie_wood",
+            () -> new Block(BlockBehaviour.Properties.of()
+                    .mapColor(MapColor.STONE)
+                    .strength(2.0f)
+                    .dynamicShape()
+                    .forceSolidOn()));
+
+    public static final RegistryObject<Block> COOKIE_LEAVES = BLOCKS.register( "cookie_leaves",
+            () -> new Block(BlockBehaviour.Properties.of()
+                    .mapColor(MapColor.STONE)
+                    .strength(0.2f)
+                    .dynamicShape()
+                    .forceSolidOn()));
+
+    public static final RegistryObject<Block> OREO_STONE = BLOCKS.register( "oreo_stone",
+            () -> new Block(BlockBehaviour.Properties.of()
+                    .mapColor(MapColor.STONE)
+                    .strength(10.0f)
+                    .dynamicShape()
+                    .forceSolidOn()));
+
+    public static final RegistryObject<Block> OREO_DIRT = BLOCKS.register( "oreo_dirt",
+            () -> new Block(BlockBehaviour.Properties.of()
+                    .mapColor(MapColor.STONE)
+                    .strength(0.9f)
+                    .dynamicShape()
+                    .forceSolidOn()));
+
+    public static final RegistryObject<Block> OREO_GRASS_BLOCK = BLOCKS.register( "oreo_grass_block",
+            () -> new Block(BlockBehaviour.Properties.of()
+                    .mapColor(MapColor.STONE)
+                    .strength(0.9f)
+                    .dynamicShape()
+                    .forceSolidOn()));
 }
