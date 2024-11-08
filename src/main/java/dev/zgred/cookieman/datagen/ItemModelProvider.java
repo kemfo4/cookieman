af802619b7ad694143f0ba75043b6d8cb37b7f84
kemfo4 <kemfomate@gmail.com>
2024-11-08 17:59:18 +0100

    nwe things

--- src/main/java/dev/zgred/cookieman/datagen/ItemModelProvider.java
+++ src/main/java/dev/zgred/cookieman/datagen/ItemModelProvider.java
@@ -1,4 +1,27 @@
 package dev.zgred.cookieman.datagen;
 
-public class ItemModelProvider {
+import dev.zgred.cookieman.Cookieman;
+import dev.zgred.cookieman.init.ItemInit;
+import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.client.model.generators.ItemModelBuilder;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ItemModelProvider extends net.minecraftforge.client.model.generators.ItemModelProvider {
+    public ItemModelProvider(PackOutput output, String modid, ExistingFileHelper existingFileHelper) {
+        super(output, modid, existingFileHelper);
+    }
+
+    @Override
+    protected void registerModels() {
+        simpleItem(ItemInit.COOKIE_INGOT);
+    }
+
+    private ItemModelBuilder simpleItem(RegistryObject<Item> item) {
+        return withExistingParent(item.getId().getPath(),
+                new ResourceLocation("item/generated")).texture("layer0",
+                new ResourceLocation(Cookieman.MODID, "item/" + item.getId().getPath()));
+    }
 }
